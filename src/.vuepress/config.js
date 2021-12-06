32ff7589d2b2e3dce6888c3b04787532db2df75a
jacobstein <jacob@stein.sh>
2021-12-05 19:55:40 -0500

    fix: add base in config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -22,6 +22,8 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
 
+  base: '/docs/',
+
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
